e8be90d8d3071a6203017bd47165163a9f05e948
DESKTOP-L943S3Q\ADMIN <phandaian0409@gmail.com>
2025-03-27 15:41:58 +0700

    đổi màu

--- app/src/main/java/com/example/sellapp/utils/Utils.java
+++ app/src/main/java/com/example/sellapp/utils/Utils.java
@@ -9,6 +9,6 @@ import java.util.List;
 public class Utils {
     public static List<GioHang> mangiohang = new ArrayList<>();
     public static List<GioHang> mangmuahang = new ArrayList<>();
-    public static final String BASE_URL = "http://192.168.0.247/sellapp/";
+    public static final String BASE_URL = "http://10.21.10.37/sellapp/";
     public static User user_current = new User();
 }
